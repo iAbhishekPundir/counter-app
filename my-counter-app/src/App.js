34dada6152d88b454cc9f844010fe0f3c7861ac3
Abhishek <pundirabhishek987@gmail.com>
2023-01-05 15:52:23 +0530

    Add: increment functionality

--- my-counter-app/src/App.js
+++ my-counter-app/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+ 
 
 function App() {
+
+  const[counter, setCounter]=useState(0);
+  function increment(){
+    setCounter(counter+1);
+  }
   return (
     <div className="App">
       <h1>Counter application</h1>
+      
+      <br></br>
+      <h2>{counter}</h2>
+      <button onClick={increment}>+</button>
+
+
     </div>
   );
 }
