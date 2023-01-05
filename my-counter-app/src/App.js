80fd6334f0aebec20460f7aee65ac755e499a71c
Abhishek <pundirabhishek987@gmail.com>
2023-01-05 15:54:26 +0530

    Add: decrement functionality

--- my-counter-app/src/App.js
+++ my-counter-app/src/App.js
@@ -10,13 +10,17 @@ function App() {
   function increment(){
     setCounter(counter+1);
   }
+  function decrement(){
+    setCounter(counter-1);
+  }
   return (
     <div className="App">
       <h1>Counter application</h1>
       
       <br></br>
       <h2>{counter}</h2>
-      <button onClick={increment}>+</button>
+      <button style={{margin:"4px"}} onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
 
 
     </div>
