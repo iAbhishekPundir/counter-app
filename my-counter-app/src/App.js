42f9a198f9b10580c1cc8f59c2ef6f1008842754
Abhishek <pundirabhishek987@gmail.com>
2023-01-05 15:56:07 +0530

    formatting

--- my-counter-app/src/App.js
+++ my-counter-app/src/App.js
@@ -1,28 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState} from 'react';
-
- 
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
 
-  const[counter, setCounter]=useState(0);
-  function increment(){
-    setCounter(counter+1);
+  function increment() {
+    setCounter(counter + 1);
   }
-  function decrement(){
-    setCounter(counter-1);
+
+  function decrement() {
+    setCounter(counter - 1);
   }
   return (
     <div className="App">
       <h1>Counter application</h1>
-      
+
       <br></br>
       <h2>{counter}</h2>
-      <button style={{margin:"4px"}} onClick={increment}>+</button>
+
+      <button style={{ margin: "4px" }} onClick={increment}>
+        +
+      </button>
+
       <button onClick={decrement}>-</button>
-
-
     </div>
   );
 }
