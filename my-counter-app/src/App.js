de0de7298472cf5eca2bb9e6e83a076c6177bef9
Abhishek <pundirabhishek987@gmail.com>
2023-01-05 15:44:00 +0530

    initial commit

---
+++ my-counter-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter application</h1>
+    </div>
+  );
+}
+
+export default App;
